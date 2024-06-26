a4346f0957326ac6e3e3bb76d28c7d1fc09ab053
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-06-26 18:04:46 -0300

    add Notification component

---
+++ src/components/Notification.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Snackbar, Alert } from "@mui/material";
+import type { Severity } from "../types";
+
+interface Props {
+  open: boolean;
+  message: string;
+  severity: Severity;
+  onClose: () => void;
+}
+
+const Notification: React.FC<Props> = ({
+  open,
+  message,
+  severity,
+  onClose,
+}) => {
+  return (
+    <Snackbar open={open} autoHideDuration={5000} onClose={onClose}>
+      <Alert onClose={onClose} severity={severity} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default Notification;
