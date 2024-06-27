dbca3b7ce9171dc73eed69ae2677b3e107e5f8ba
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-06-27 01:14:58 -0300

    create TaskItem Component

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,305 +1,22 @@
-import React, { useState, useCallback, CSSProperties } from "react";
-import {
-  ListItem,
-  ListItemText,
-  IconButton,
-  Checkbox,
-  TextField,
-  ListItemIcon,
-  Divider,
-  Stack,
-  Tooltip,
-  Button,
-  useMediaQuery,
-  useTheme,
-  Box,
-  Typography,
-} from "@mui/material";
-import {
-  Edit as EditIcon,
-  Delete as DeleteIcon,
-  Save as SaveIcon,
-  Cancel as CancelIcon,
-} from "@mui/icons-material";
-import { doc, updateDoc, deleteDoc } from "firebase/firestore";
-import db from "../firebase";
-import type { Severity, Task } from "../types";
-import { truncateText } from "../utils";
+import React from "react";
+import { ListItem, Divider, Box } from "@mui/material";
+import type { Task } from "@/types";
+import { useTaskItem } from "@/hooks/useTaskItem";
 import Dialog from "./Dialog";
+import { useNotification } from "@/hooks/useNotification";
+import { useResponsiveLayout } from "@/hooks/useResponsiveLayout";
+import EditForm from "./EditForm";
+import TaskContent from "./TaskContent";
+import TaskActions from "./TaskActions";
 
 interface Props {
   task: Task;
-  onNotification: (message: string, severity: Severity) => void;
 }
 
-const TaskItem: React.FC<Props> = ({ task, onNotification }) => {
-  const [editing, setEditing] = useState(false);
-  const [editTitle, setEditTitle] = useState(task.title);
-  const [editDescription, setEditDescription] = useState(task.description);
-  const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false);
-
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-
-  // functions to handle task actions
-  const handleComplete = useCallback(async () => {
-    try {
-      await updateDoc(doc(db, "tasks", task.id), {
-        completed: !task.completed,
-      });
-      onNotification("Tarea actualizada exitosamente", "success");
-    } catch (error) {
-      console.error("Error updating document: ", error);
-      onNotification("Error al actualizar la tarea", "error");
-    }
-  }, [task.id, task.completed, onNotification]);
-
-  const handleEdit = useCallback(() => {
-    if (!task.completed) {
-      setEditing(true);
-    } else {
-      onNotification("No puedes editar una tarea completada", "warning");
-    }
-  }, [task.completed, onNotification]);
-
-  const handleDeleteClick = useCallback(() => setDeleteConfirmOpen(true), []);
-
-  const handleDeleteConfirm = useCallback(async () => {
-    try {
-      await deleteDoc(doc(db, "tasks", task.id));
-      onNotification("Tarea eliminada exitosamente", "success");
-    } catch (error) {
-      console.error("Error deleting document: ", error);
-      onNotification("Error al eliminar la tarea", "error");
-    }
-    setDeleteConfirmOpen(false);
-  }, [task.id, onNotification]);
-
-  const handleDeleteCancel = useCallback(() => setDeleteConfirmOpen(false), []);
-
-  const handleSave = useCallback(async () => {
-    try {
-      if (!editTitle || editTitle.trim() === "") {
-        onNotification("El título de la tarea no puede estar vacío", "warning");
-        return;
-      }
-      if (!editDescription || editDescription.trim() === "") {
-        onNotification(
-          "La descripción de la tarea no puede estar vacía",
-          "warning"
-        );
-        return;
-      }
-      if (
-        editTitle.trim() === task.title.trim() ||
-        editDescription.trim() === task.description.trim()
-      ) {
-        onNotification("No se realizaron cambios en la tarea", "warning");
-        setEditing(false);
-        return;
-      }
-
-      await updateDoc(doc(db, "tasks", task.id), {
-        title: editTitle,
-        description: editDescription,
-      });
-      setEditing(false);
-      onNotification("Tarea actualizada exitosamente", "success");
-    } catch (error) {
-      console.error("Error updating document: ", error);
-      onNotification("Error al actualizar la tarea", "error");
-    }
-  }, [task.id, editTitle, editDescription, onNotification]);
-
-  const handleCancel = useCallback(() => {
-    setEditTitle(task.title);
-    setEditDescription(task.description);
-    setEditing(false);
-  }, [task.title, task.description]);
-
-  // text style for task content when completed
-  const getTextStyle = (completed: boolean): CSSProperties => ({
-    textDecoration: completed ? "line-through" : "none",
-  });
-
-  // actions buttons and icons for mobile and desktop views
-  const renderActions = useCallback(() => {
-    if (editing) {
-      return (
-        <Stack
-          spacing={1}
-          direction={isMobile ? "column" : "row"}
-          width={isMobile ? "100%" : "auto"}
-        >
-          {isMobile ? (
-            <>
-              <Button
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={handleSave}
-                startIcon={<SaveIcon />}
-              >
-                Guardar
-              </Button>
-              <Button
-                fullWidth
-                variant="outlined"
-                color="secondary"
-                onClick={handleCancel}
-                startIcon={<CancelIcon />}
-              >
-                Cancelar
-              </Button>
-            </>
-          ) : (
-            <>
-              <IconButton
-                edge="end"
-                aria-label="save"
-                title="Guardar tarea"
-                onClick={handleSave}
-                size="large"
-              >
-                <SaveIcon />
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="cancel"
-                title="Cancelar edición"
-                onClick={handleCancel}
-                size="large"
-              >
-                <CancelIcon />
-              </IconButton>
-            </>
-          )}
-        </Stack>
-      );
-    }
-    return (
-      <Stack
-        spacing={1}
-        direction={isMobile ? "column" : "row"}
-        width={isMobile ? "100%" : "auto"}
-      >
-        {isMobile ? (
-          <>
-            <Button
-              fullWidth
-              variant="outlined"
-              color="primary"
-              onClick={handleEdit}
-              startIcon={<EditIcon />}
-            >
-              Editar
-            </Button>
-            <Button
-              fullWidth
-              variant="outlined"
-              color="secondary"
-              onClick={handleDeleteClick}
-              startIcon={<DeleteIcon />}
-            >
-              Eliminar
-            </Button>
-          </>
-        ) : (
-          <>
-            <IconButton
-              edge="end"
-              aria-label="edit"
-              title="Editar tarea"
-              onClick={handleEdit}
-            >
-              <EditIcon />
-            </IconButton>
-            <IconButton
-              edge="end"
-              aria-label="delete"
-              title="Eliminar tarea"
-              onClick={handleDeleteClick}
-            >
-              <DeleteIcon />
-            </IconButton>
-          </>
-        )}
-      </Stack>
-    );
-  }, [
-    editing,
-    isMobile,
-    handleSave,
-    handleCancel,
-    handleEdit,
-    handleDeleteClick,
-  ]);
-
-  // edit form with textfields
-  const renderEditForm = () => (
-    <Box width="100%">
-      <TextField
-        value={editTitle}
-        onChange={(e) => setEditTitle(e.target.value)}
-        fullWidth
-        margin="normal"
-        autoFocus
-      />
-      <TextField
-        value={editDescription}
-        onChange={(e) => setEditDescription(e.target.value)}
-        fullWidth
-        multiline
-        margin="normal"
-      />
-      {isMobile && (
-        <Box width="100%" mt={2}>
-          {renderActions()}
-        </Box>
-      )}
-    </Box>
-  );
-
-  // task content with checkbox
-  const renderTaskContent = () => (
-    <Box display="flex" alignItems="center" width="100%">
-      <ListItemIcon>
-        <Checkbox
-          edge="start"
-          checked={task.completed}
-          onChange={handleComplete}
-        />
-      </ListItemIcon>
-      <ListItemText
-        primary={
-          <Typography component="p" style={getTextStyle(task.completed)}>
-            {task.title.length > 40 ? (
-              <Tooltip title={task.title} arrow placement="top-start">
-                <span>{truncateText(task.title, 20)}</span>
-              </Tooltip>
-            ) : (
-              task.title
-            )}
-          </Typography>
-        }
-        secondary={
-          <Typography
-            component="p"
-            variant="body2"
-            style={getTextStyle(task.completed)}
-          >
-            {task.description.length > 50 ? (
-              <Tooltip title={task.description} arrow placement="top-start">
-                <span>{truncateText(task.description, 25)}</span>
-              </Tooltip>
-            ) : (
-              task.description
-            )}
-          </Typography>
-        }
-      />
-    </Box>
-  );
+const TaskItem: React.FC<Props> = React.memo(({ task }) => {
+  const isMobile = useResponsiveLayout();
+  const { handleNotification } = useNotification();
+  const taskItemProps = useTaskItem(task, handleNotification);
 
   return (
     <>
@@ -317,29 +34,33 @@ const TaskItem: React.FC<Props> = ({ task, onNotification }) => {
           flexGrow={1}
           width={isMobile ? "100%" : "auto"}
         >
-          {editing ? renderEditForm() : renderTaskContent()}
-          {isMobile && !editing && (
+          {taskItemProps.editing ? (
+            <EditForm {...taskItemProps} />
+          ) : (
+            <TaskContent task={task} {...taskItemProps} />
+          )}
+          {isMobile && (
             <Box width="100%" mt={2}>
-              {renderActions()}
+              <TaskActions {...taskItemProps} />
             </Box>
           )}
         </Box>
         {!isMobile && (
           <Box ml={2} display="flex" alignItems="center">
-            {renderActions()}
+            <TaskActions {...taskItemProps} />
           </Box>
         )}
       </ListItem>
       <Divider variant="fullWidth" aria-hidden="true" />
       <Dialog
-        open={deleteConfirmOpen}
-        onClose={handleDeleteCancel}
+        open={taskItemProps.deleteConfirmOpen}
+        onClose={taskItemProps.handleDeleteCancel}
         title="Confirmar eliminación"
         content="¿Estás seguro de que quieres eliminar esta tarea?"
-        onConfirm={handleDeleteConfirm}
+        onConfirm={taskItemProps.handleDeleteConfirm}
       />
     </>
   );
-};
+});
 
 export default TaskItem;
